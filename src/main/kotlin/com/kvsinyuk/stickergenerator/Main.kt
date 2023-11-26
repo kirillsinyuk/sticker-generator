bd37b6f5168c85309e69b9f1f75a42a8f3c27633
Kirill Sinyuk <kirill.s.works@gmail.com>
2023-11-26 10:00:18 +0300

    init commit

---
+++ src/main/kotlin/com/kvsinyuk/stickergenerator/Main.kt
@@ -0,0 +1,11 @@
+package com.kvsinyuk.stickergenerator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StickerGeneratorApplication
+
+fun main(args: Array<String>) {
+	runApplication<StickerGeneratorApplication>(*args)
+}
---
+++ src/test/kotlin/com/kvsinyuk/stickergenerator/StickerGeneratorApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kvsinyuk.stickergenerator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StickerGeneratorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
