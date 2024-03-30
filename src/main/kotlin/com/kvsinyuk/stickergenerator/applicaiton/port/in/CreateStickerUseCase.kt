380c12ce7a03dd0bdbe0f811212eed03a161b12b
Kirill Sinyuk <kirill.s.works@gmail.com>
2024-03-30 07:56:02 +0300

    #73 delete removeBackground feature from sticker creation

--- src/main/kotlin/com/kvsinyuk/stickergenerator/applicaiton/port/in/CreateStickerUseCase.kt
+++ src/main/kotlin/com/kvsinyuk/stickergenerator/applicaiton/port/in/CreateStickerUseCase.kt
@@ -1,8 +1,6 @@
 package com.kvsinyuk.stickergenerator.applicaiton.port.`in`
 
-import com.kvsinyuk.stickergenerator.applicaiton.port.out.http.RemoveBackgroundPort
 import com.kvsinyuk.stickergenerator.applicaiton.service.AddTextService
-import com.kvsinyuk.stickergenerator.applicaiton.service.CropImageService
 import com.kvsinyuk.stickergenerator.applicaiton.service.PadImageService
 import com.kvsinyuk.stickergenerator.applicaiton.service.ResizeImageService
 import com.kvsinyuk.stickergenerator.applicaiton.utils.getBufferedImage
@@ -16,18 +14,14 @@ interface CreateStickerUseCase {
 
 @Component
 class CreateStickerUseCaseImpl(
-    private val removeBackgroundPort: RemoveBackgroundPort,
-    private val cropImageService: CropImageService,
     private val resizeImageService: ResizeImageService,
     private val addTextService: AddTextService,
     private val padImageService: PadImageService,
 ) : CreateStickerUseCase {
     override fun createSticker(botData: BotData): BufferedImage {
         val stickerData = botData.getAsCreateStickerData()
-        return removeBackgroundPort.removeBackground(stickerData.image!!.getBufferedImage(), stickerData.originalFilename)
-            .let { cropImageService.cropImage(it) }
+        return resizeImageService.resizeBufferedImage(stickerData.image!!.getBufferedImage())
             .let { padImageService.addPaddingIfNecessary(it, stickerData.topText.isBlank()) }
-            .let { resizeImageService.resizeBufferedImage(it) }
             .let { addTextService.addText(it, botData.getAsCreateStickerData().topText, true) }
             .let { addTextService.addText(it, botData.getAsCreateStickerData().bottomText, false) }
     }
